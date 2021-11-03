926659fdd59c5429c7564b3cbd00385bd904ae87
Vida May Kotico <vmkotico@yahoo.com>
2021-11-03 10:45:10 -0700

    commit tree 4b09f7b9a4a7e909bbda2cef7510e9290a81e9dc

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,34 @@
-# your code goes here
+require 'pry'
+
+class Person
+    attr_reader :name, :happiness
+    attr_accessor :bank_account
+    def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+        @hygiene = hygiene
+      
+    end
+
+# def name
+#     @name
+   # end
+
+
+# def bank_account = (value)
+#     @bank_account = value 
+# end
+
+def happiness=(new_happiness)
+    if new_happiness >=0 && new_happiness <=10
+    @happiness = new_happiness
+    end 
+end
+
+vida = Person.new("Vida")
+
+puts vida.name 
+
+end
+
